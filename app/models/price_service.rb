101aebb4046eacf128928574e4a7b0bdbc65b81a
Alexandre Calaça <alexcalaca@gmail.com>
2023-01-15 15:13:22 -0300

    feat: create alphavantage::client
    Alphavantage::Client is used to create a client that will be used from the AlphavantageRB to contact the website.

--- app/models/price_service.rb
+++ app/models/price_service.rb
@@ -1,4 +1,9 @@
 require 'byebug'
 
 class PriceService
+  def self.client
+    @client ||= Alphavantage::Client.new(
+      key: Rails.application.credentials.dig(:alphavantage, :key)
+    )
+  end
 end
