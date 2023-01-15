703c0d832355ffa39023edb9158f0bb864906af4
Alexandre Calaça <alexcalaca@gmail.com>
2023-01-15 15:14:54 -0300

    feat: retrieve historical data of a stock

--- app/models/price_service.rb
+++ app/models/price_service.rb
@@ -1,9 +1,21 @@
 require 'byebug'
 
 class PriceService
+  def self.fetch_prices(ticker)
+    date_and_close_pairs = client.
+      stock(symbol: ticker).
+      timeseries(adjusted: true, outputsize: "full").
+      output["Time Series (Daily)"].
+      map { |k, v| [k, v["5. adjusted close"]] }
+    Hash[date_and_close_pairs]
+    #byebug
+    #1+1    
+  end
+
   def self.client
     @client ||= Alphavantage::Client.new(
       key: Rails.application.credentials.dig(:alphavantage, :key)
     )
   end
+  
 end
